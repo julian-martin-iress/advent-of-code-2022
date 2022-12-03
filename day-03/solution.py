1d5cb012dbdebcc055ed2f8578fe80219733d1c9
Julian Martin <julian.martin@iress.com>
2022-12-03 12:57:36 +0000

    day 3!

---
+++ day-03/solution.py
@@ -0,0 +1,37 @@
+import string
+
+alphabet_mapping = list(string.ascii_letters)
+
+def score_for(char):
+    return alphabet_mapping.index(char) + 1
+
+lines = open('input.txt', 'r').readlines()
+
+score_part_1 = 0
+
+for line in lines:
+    firstpart, secondpart = line[:len(line)//2], line[len(line)//2:]
+    for char in firstpart:
+        if char in secondpart:
+            score_part_1 += score_for(char)
+            break
+
+print(score_part_1)
+
+# part 2
+score_part_2 = 0
+i = 0
+
+while i < len(lines):
+    rucksack1 = lines[i]
+    rucksack2 = lines[i + 1]
+    rucksack3 = lines[i + 2]
+
+    for char in rucksack1:
+        if char in rucksack2 and char in rucksack3: 
+            score_part_2 += score_for(char)
+            break
+
+    i += 3
+
+print(score_part_2)
