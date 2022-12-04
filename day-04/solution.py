d010a2509f05bbd98096b48bd7de72472e96bfc3
Julian Martin <julian.martin@iress.com>
2022-12-04 09:27:22 +0000

    tidy

--- day-04/solution.py
+++ day-04/solution.py
@@ -1,12 +1,10 @@
 lines = open('input.txt', 'r').readlines()
 
-score_part_1 = 0
-score_part_2 = 0
+score_part_1, score_part_2 = 0, 0
 
 for line in lines:
     start1, end1, start2, end2 = map(int, line.replace(",", "-").split("-"))
     score_part_1 += 1 if (start1 <= start2 and end1 >= end2 ) or (start2 <= start1 and end2 >= end1) else 0
     score_part_2 += 0 if (end1 < start2 ) or (end2 < start1) else 1
 
-print(score_part_1)
-print(score_part_2)
+print(score_part_1, score_part_2)
