25852a9a53647028aac55463c81ad008a83e6732
Julian Martin <julian.martin@iress.com>
2022-12-05 09:41:01 +0000

    initial day 5

---
+++ day-05/solution.py
@@ -0,0 +1,46 @@
+def run_it(crate_mover_model):
+
+    mode = "stacks"
+    stacks_data = list()
+
+    for line in open('input.txt', 'r').readlines():
+        if line.rstrip() == '':
+            # end of reading stacks
+            mode = "commands"
+            # remove last item (numbers row)
+            stacks_data.pop()
+            # build a list for each stack
+            stacks_data.reverse()
+            stacks = list()
+            for x in range(9):
+                stacks.append(list())
+            
+            for item in stacks_data:
+                col = 0
+                for char in item:
+                    col += 1
+                    if char != " ":
+                        colToAdd = ((col + 2)//4)-1
+                        stacks[colToAdd].append(char)
+            continue
+
+        if mode == "stacks":
+            stacks_data.append(line.replace("[", " ").replace("]"," ").replace("\n", ""))
+
+        if mode == "commands":
+            how_many, from_stack, to_stack = map(int, line.replace("move ", "").replace("from ", "").replace("to ", "").split(" "))
+            # do the move
+            item_to_move = stacks[from_stack-1][-how_many:]
+            if (crate_mover_model == 9000):
+                item_to_move.reverse()
+            stacks[from_stack-1] = stacks[from_stack-1][:-how_many]
+            stacks[to_stack-1].extend(item_to_move)
+        
+    top_items = ""
+    for stack in stacks:
+        top_items += stack[-1:][0]
+    
+    return top_items
+
+print(run_it(9000)) #TPGVQPFDH
+print(run_it(9001)) #DMRDFRHHH
