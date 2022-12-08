cb645b0f1c455ecd4c9808afe2fde098e9608d7c
Julian Martin <julian.martin@iress.com>
2022-12-08 19:06:09 +0000

    tidy up a bit

--- day-08/solution.py
+++ day-08/solution.py
@@ -15,9 +15,21 @@ def transform_to_columns(rows):
 
     return columns
 
-def is_tree_visible(my_tree, trees_in_line):
-    ''' determines whether tree is taller than all trees in the line '''
-    return all(int(my_tree) > int(i) for i in trees_in_line)
+def get_trees_in_all_directions(current_row, row_idx, current_column, col_idx):
+    ''' gets the trees in all 4 directions from the current tree position '''
+    tr_left = current_row[0:col_idx][::-1] # [::-1] reverses the string
+    tr_right = current_row[col_idx + 1:]
+    tr_up = current_column[0:row_idx][::-1] # [::-1] reverses the string
+    tr_down = current_column[row_idx + 1:]
+
+    return tr_left, tr_right, tr_up, tr_down
+
+def is_tree_visible(my_tree, tree_lists):
+    ''' determines whether my tree is taller than all the trees in any of the lines '''
+    for trees_in_line in tree_lists:
+        if all(int(my_tree) > int(i) for i in trees_in_line):
+            return True
+    return False
 
 def distance_viewable(my_tree, trees_in_line):
     ''' determines how far along the line is visible from the current tree '''
@@ -44,15 +56,8 @@ for row_index, row in enumerate(_rows):
             continue
 
         # This is an "inner tree"
-        trees_left = row[0:column_index][::-1] # [::-1] reverses the string
-        trees_right = row[column_index + 1:]
-        trees_up = _columns[column_index][0:row_index] # [::-1] reverses the string
-        trees_down = _columns[column_index][row_index + 1:]
-
-        if is_tree_visible(tree, trees_left) or \
-            is_tree_visible(tree, trees_right) or \
-            is_tree_visible(tree, trees_up) or \
-            is_tree_visible(tree, trees_down):
+        left, right, up, down = get_trees_in_all_directions(row, row_index, _columns[column_index], column_index)
+        if is_tree_visible(tree, [left, right, up, down]):
             visible_count += 1
 
 # part 1
@@ -62,10 +67,8 @@ print(visible_count) # 1543
 tree_scores = list()
 for row_index, row in enumerate(_rows):
     for column_index, tree in enumerate(row):
-        trees_left = row[0:column_index][::-1] # [::-1] reverses the string
-        trees_right = row[column_index + 1:]
-        trees_up = _columns[column_index][0:row_index][::-1] # [::-1] reverses the string
-        trees_down = _columns[column_index][row_index + 1:]
+        trees_left, trees_right, trees_up, trees_down = \
+            get_trees_in_all_directions(row, row_index, _columns[column_index], column_index)
 
         left = distance_viewable(tree, trees_left)
         right = distance_viewable(tree, trees_right)
